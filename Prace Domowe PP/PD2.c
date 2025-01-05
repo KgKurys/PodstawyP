8889f30f0b7a7c0f81c5bde52827e074c0b97697
KgKurys <kg.grap@gmail.com>
2025-01-05 14:37:16 +0100

    Add functionality to write read books above average price and find most expensive unread book by author; update output messages to Polish

--- Prace Domowe PP/PD2.c
+++ Prace Domowe PP/PD2.c
@@ -20,14 +20,10 @@ void findMostExpensiveUnreadBookByAuthor(Book books[], int n, const char* author
 
 int main() {
     FILE *file = fopen("biblioteka.txt", "r");
-    if (!file) {
-        perror("Error opening file");
-        return EXIT_FAILURE;
-    }
 
     int n;
     fscanf(file, "%d", &n);
-    printf("Number of books: %d\n", n);
+    printf("Ilosc ksiazek: %d\n", n);
     Book *books = malloc(n * sizeof(Book));
     
     for (int i = 0; i < n; i++) {
@@ -45,6 +41,32 @@ int main() {
     displayBooks(books, n);
     findCheapestBook(books, n);
     findThickestBooksByCategory(books,n);
+    
+    char filename[100];    
+    printf("Przeczytane ksiazki powyzej sredniej ceny, podaj nazwe pliku: ");
+    scanf("%99s", filename);
+    printf("\n");
+    
+    writeReadBooksAboveAveragePrice(books,n,filename);
+    char author[30];
+    while (1){
+        printf("Dla jakiego autora chcesz znalezc ksiazke\n");
+        printf("\n");
+        scanf("%29s", author);
+        if (strcmp(author, "Agatha_Christie") == 0 ||
+            strcmp(author, "Suzanne_Collins") == 0 ||
+            strcmp(author, "Stephane_Meyer") == 0 ||
+            strcmp(author, "Brandon_Sanderson") == 0 ||
+            strcmp(author, "J_Rowling") == 0 ||
+            strcmp(author, "Neil_Gaiman") == 0) {
+    break;
+        } else{
+            printf("nieznany autor\n");
+        }
+    }
+    printf("\n");
+    
+    findMostExpensiveUnreadBookByAuthor(books,n,author);
     free(books);
     return 0;
 }
@@ -72,7 +94,7 @@ void findCheapestBook(Book books[], int n){
         }
         
     }
-    printf("Cheapest book: %s (%.2f)\n",
+    printf("Najtansza ksiazka to: %s (%.2f)\n",
            books[index].tytul,
            books[index].cena);
 printf("\n");
@@ -99,12 +121,62 @@ void findThickestBooksByCategory(Book books[], int n){
                 }
             }
             
-            printf("Thickest in %s: %s (%d pages)\n",
+            printf("Najgrubsza ksiazka w kategorii %s to: %s (%d stron)\n",
                    books[maxIndex].kat,
                    books[maxIndex].tytul,
                    books[maxIndex].strony);
         }
     }
+printf("\n");    
+}
+void writeReadBooksAboveAveragePrice(Book books[],int n, const char* filename){
+    float overallprice = 0;
     
-     
+    for (int i = 0; i<n; i++){
+        overallprice += books[i].cena;
+    }
+    float avargeprice = overallprice / n;
+    
+    FILE *write = fopen(filename,"w");
+
+    for (int i = 0; i<n; i++){
+        if (books[i].read == 1 && books[i].cena > avargeprice){
+            
+            fprintf(write,"%-20s %-40s %-20s %-10.2f %-10d %-10d %-10d\n",
+               books[i].autor,
+               books[i].tytul,
+               books[i].kat,
+               books[i].cena,
+               books[i].strony,
+               books[i].read,
+               books[i].regal);
+
+
+        }
+    }
+    fflush(write);
+    fclose(write);
+}
+
+void findMostExpensiveUnreadBookByAuthor(Book books[], int n, const char* author) {
+    int mostExpIndex = -1;
+    float highestPrice = 0.0f;
+
+    for (int i = 0; i < n; i++) {
+        if (strcmp(books[i].autor, author) == 0 && books[i].read == 0) {
+            if (books[i].cena > highestPrice) {
+                highestPrice = books[i].cena;
+                mostExpIndex = i;
+            }
+        }
+    }
+
+    if (mostExpIndex == -1) {
+        printf("Brak nieprzeczytanych ksiazek tego autora: %s\n", author);
+    } else {
+        printf("Najdrozsza nieprzeczytana ksiazka  %s to \"%s\" za cene %.2f\n",
+               author,
+               books[mostExpIndex].tytul,
+               highestPrice);
+    }
 }
