9d07b6aa90cd6eeb2d8465d9ed82e0183c3ed0bd
KgKurys <kg.grap@gmail.com>
2025-03-04 18:07:20 +0100

    Update Visual Studio solution to include new projects and configurations

--- analiza_temperatur/obj/Debug/net9.0/analiza_temperatur.AssemblyInfo.cs
+++ analiza_temperatur/obj/Debug/net9.0/analiza_temperatur.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("analiza_temperatur")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff8c1912acb0859a27a79f05d3e35b1b344d48ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ebf74607671a5ae672ee4d1da40dc8583abb69")]
 [assembly: System.Reflection.AssemblyProductAttribute("analiza_temperatur")]
 [assembly: System.Reflection.AssemblyTitleAttribute("analiza_temperatur")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
