30bbc333038314743185d76e26c03868af919c6c
KgKurys <kg.grap@gmail.com>
2025-03-11 17:06:10 +0100

    dodanie worksapce

--- Obiektowe/hello_world/obj/Debug/net9.0/hello_world.AssemblyInfo.cs
+++ Obiektowe/hello_world/obj/Debug/net9.0/hello_world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34dd2146e0b1e0211482c4462acf75fd2faa5db1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485c614f56444c344a588ba25828c6e9e7cf8838")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
