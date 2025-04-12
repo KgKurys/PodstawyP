b778871f11d938258f1c1469abbfafac6d9f3b24
KgKurys <kg.grap@gmail.com>
2025-04-12 11:46:05 +0200

    Update assembly informational versions for analiza_temperatur, hello_world, and todomanager applications

--- Obiektowe/analiza_temperatur/obj/Debug/net9.0/analiza_temperatur.AssemblyInfo.cs
+++ Obiektowe/analiza_temperatur/obj/Debug/net9.0/analiza_temperatur.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("analiza_temperatur")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffce5d1a3bd6d32486fa5fe2af1a12cb7e8cb27f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8dd70428e6a382512b7ff32c075cad750cbb753")]
 [assembly: System.Reflection.AssemblyProductAttribute("analiza_temperatur")]
 [assembly: System.Reflection.AssemblyTitleAttribute("analiza_temperatur")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
